e3045650d9e16a0de1e544862f972e9794c02ebb
Santiago Tassara <santiago.tassara@hotmail.com>
2018-09-22 13:40:48 -0300

    Entidades para armar Encuesta / Mensajes / Novedades

---
+++ InnovaLED/Entidades/Pregunta.vb
@@ -0,0 +1,47 @@
+﻿Public Class Pregunta
+
+    Public Enum eTipoPregunta
+        Opinion = 1
+        Desarrollo = 2
+    End Enum
+
+    Private _idPregunta As Integer
+    Public Property ID_Pregunta() As Integer
+        Get
+            Return _idPregunta
+        End Get
+        Set(ByVal value As Integer)
+            _idPregunta = value
+        End Set
+    End Property
+
+    Private _enunciado As String
+    Public Property Enunciado() As String
+        Get
+            Return _enunciado
+        End Get
+        Set(ByVal value As String)
+            _enunciado = value
+        End Set
+    End Property
+
+    Private _tipoPregunta As eTipoPregunta
+    Public Property tipoPregunta() As eTipoPregunta
+        Get
+            Return _tipoPregunta
+        End Get
+        Set(ByVal value As eTipoPregunta)
+            _tipoPregunta = value
+        End Set
+    End Property
+
+    Private _respuesta As Respuesta
+    Public Property respuesta() As Respuesta
+        Get
+            Return _respuesta
+        End Get
+        Set(ByVal value As Respuesta)
+            _respuesta = value
+        End Set
+    End Property
+End Class
