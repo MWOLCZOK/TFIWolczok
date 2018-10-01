c5090de659cffee6da824e2440e1d49825a7e964
mwolczok <matias.wolczok@gmail.com>
2018-09-30 22:54:12 -0300

    Template Mail

--- InnovaLED/Negocio/MaillingBLL.vb
+++ InnovaLED/Negocio/MaillingBLL.vb
@@ -10,18 +10,18 @@ Public Class MaillingBLL
         Try
             Dim Correo As New System.Net.Mail.MailMessage()
             Correo.Attachments.Add(New Attachment(ruta & "\twitter.png") With {.ContentId = "twitter"})
-            Correo.Attachments.Add(New Attachment(ruta & "\remote.png") With {.ContentId = "logo"}) ' Es la imagen del titulo
-            Correo.Attachments.Add(New Attachment(ruta & "\knight.png") With {.ContentId = "knight"}) ' Es la imagen deo cuerpo (espadas)
+            Correo.Attachments.Add(New Attachment(ruta & "\bulb.png") With {.ContentId = "logo"}) ' Es la imagen del titulo
+            Correo.Attachments.Add(New Attachment(ruta & "\luz-led.png") With {.ContentId = "knight"}) ' Es la imagen deo cuerpo (espadas)
             Correo.Attachments.Add(New Attachment(ruta & "\facebook.png") With {.ContentId = "facebook"})
             Correo.Attachments.Add(New Attachment(ruta & "\blue.png") With {.ContentId = "lkdn"})
             Correo.Attachments.Add(New Attachment(ruta & "\red.png") With {.ContentId = "pint"})
 
-            Correo.Attachments.Add(New Attachment(ruta & "\youtube-gaming_1200x500.jpg") With {.ContentId = "banner"}) ' Es el banner, medio del mail.
+            Correo.Attachments.Add(New Attachment(ruta & "\bannermail.jpg") With {.ContentId = "banner"}) ' Es el banner, medio del mail.
             Dim variable As String() = body.Split("$$$")
             variable(0) = variable(0) & ubicacionserver & "/ConfirmarRegistracion.aspx?tok=" & token
             body = variable(0) & variable(3)
             Correo.IsBodyHtml = True
-            Correo.To.Add("Matias.Wolczok2@gmail.com")
+            Correo.To.Add("matias.wolczok2@gmail.com")
             Correo.Subject = "InnovaLED - Registro de Usuario"
             Correo.Body = body
             Correo.Priority = System.Net.Mail.MailPriority.Normal
@@ -36,19 +36,19 @@ Public Class MaillingBLL
         Try
             Dim Correo As New System.Net.Mail.MailMessage()
             Correo.Attachments.Add(New Attachment(ruta & "\twitter.png") With {.ContentId = "twitter"})
-            Correo.Attachments.Add(New Attachment(ruta & "\remote.png") With {.ContentId = "logo"})
-            Correo.Attachments.Add(New Attachment(ruta & "\game-console.png") With {.ContentId = "game-console"})
+            Correo.Attachments.Add(New Attachment(ruta & "\bulb.png") With {.ContentId = "logo"})
+            Correo.Attachments.Add(New Attachment(ruta & "\idea.png") With {.ContentId = "game-console"})
             Correo.Attachments.Add(New Attachment(ruta & "\facebook.png") With {.ContentId = "facebook"})
             Correo.Attachments.Add(New Attachment(ruta & "\blue.png") With {.ContentId = "lkdn"})
             Correo.Attachments.Add(New Attachment(ruta & "\red.png") With {.ContentId = "pint"})
 
-            Correo.Attachments.Add(New Attachment(ruta & "\youtube-gaming_1200x500.jpg") With {.ContentId = "banner"})
+            Correo.Attachments.Add(New Attachment(ruta & "\bannermail.jpg") With {.ContentId = "banner"})
             Dim variable As String() = body.Split("$$$")
             variable(0) = variable(0) & ubicacionserver & "/ConfirmarRecupero.aspx?tok=" & token
             body = variable(0) & variable(3)
             Correo.IsBodyHtml = True
             Correo.To.Add("matias.wolczok2@gmail.com")
-            Correo.Subject = "InnovaLED - Registro de Usuario"
+            Correo.Subject = "InnovaLED - Recuperación de contraseña de Usuario"
             Correo.Body = body
             Correo.Priority = System.Net.Mail.MailPriority.Normal
             Dim smtp As New System.Net.Mail.SmtpClient
