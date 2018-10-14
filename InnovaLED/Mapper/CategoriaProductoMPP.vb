3369ef9497510cd560a02f5b30aa3b9dd4ff1e28
SantaT89 <santa.tassara@gmail.com>
2018-10-14 19:23:41 -0300

    CategoriaProductoMPP, LineaProductoMPP

---
+++ InnovaLED/Mapper/CategoriaProductoMPP.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SqlClient
+Imports DAL
+Imports Entidades
+
+
+
+Public Class CategoriaProductoMPP
+
+
+
+    Public Function obtenerLineaProducto(ByVal categoria_producto As Entidades.CategoriaProducto) As Entidades.CategoriaProducto
+        Try
+            Dim consulta As String = "Select * from CategoriaProducto where ID_Categoria=@ID_Categoria"
+            Dim Command As SqlCommand = Acceso.MiComando(consulta)
+            With Command.Parameters
+                .Add(New SqlParameter("@ID_Linea", categoria_producto.ID_Categoria))
+            End With
+            Dim dt As DataTable = Acceso.Lectura(Command)
+            If dt.Rows.Count = 1 Then
+                FormatearLineaProducto(categoria_producto, dt.Rows(0))
+                Return categoria_producto
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+    End Function
+
+
+
+
+    Public Sub FormatearLineaProducto(ByVal categoria_producto As Entidades.CategoriaProducto, ByVal row As DataRow)
+        Try
+            categoria_producto.ID_Categoria = row("ID_Categoria")
+            categoria_producto.Descripcion = row("Descripcion")
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+
+
+End Class
---
+++ InnovaLED/Mapper/LineaProductoMPP.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SqlClient
+Imports DAL
+Imports Entidades
+
+
+
+Public Class LineaProductoMPP
+
+
+
+    Public Function obtenerLineaProducto(ByVal linea_producto As Entidades.LineaProducto) As Entidades.LineaProducto
+        Try
+            Dim consulta As String = "Select * from LineaProducto where ID_Linea=@ID_Linea"
+            Dim Command As SqlCommand = Acceso.MiComando(consulta)
+            With Command.Parameters
+                .Add(New SqlParameter("@ID_Linea", linea_producto.ID_Linea))
+            End With
+            Dim dt As DataTable = Acceso.Lectura(Command)
+            If dt.Rows.Count = 1 Then
+                FormatearLineaProducto(linea_producto, dt.Rows(0))
+                Return linea_producto
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+    End Function
+
+
+
+
+    Public Sub FormatearLineaProducto(ByVal linea_Producto As Entidades.LineaProducto, ByVal row As DataRow)
+        Try
+            linea_Producto.ID_Linea = row("ID_Linea")
+            linea_Producto.Descripcion = row("Descripcion")
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+
+
+End Class
