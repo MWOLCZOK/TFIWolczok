ad67332fde4ac29299773c8cf9413b6f658711c1
Marcos <marcos.tassara@gmail.com>
2018-12-01 20:54:02 -0300

    Revert "Revert "CHAT""
    This reverts commit 39e64c830dcfcc7f768c9c97aa3b34d0f42c42a6.

---
+++ InnovaLED/Mapper/ChatMPP.vb
@@ -0,0 +1,129 @@
+﻿Imports DAL
+Imports Entidades
+Imports System.Data.SqlClient
+
+
+
+
+Public Class ChatMPP
+
+
+    Public Function GenerarMensaje(chat As ChatEntidad) As Boolean
+        Try
+            Dim Command As SqlCommand = Acceso.MiComando("insert into Chat (ID_Usuario,Finalizado)  Output Inserted.ID_Chat values (@ID_Usuario, @Finalizado)")
+            With Command.Parameters
+                .Add(New SqlParameter("@ID_Usuario", chat.Usuario.ID_Usuario))
+                .Add(New SqlParameter("@Finalizado", False))
+            End With
+
+            chat.ID = Acceso.Scalar(Command)
+
+            Dim Command2 As SqlCommand = Acceso.MiComando("insert into Mensaje_Chat (ID_Chat,ID_Usuario,Fecha,Texto) values (@ID_Chat,@ID_Usuario, @Fecha,@Texto)")
+            With Command2.Parameters
+                .Add(New SqlParameter("@ID_Chat", chat.ID))
+                .Add(New SqlParameter("@ID_Usuario", chat.Usuario.ID_Usuario))
+                .Add(New SqlParameter("@Fecha", chat.MensajesChat.First.Fecha))
+                .Add(New SqlParameter("@Texto", chat.MensajesChat.First.Texto))
+            End With
+            Acceso.Escritura(Command2)
+
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Sub AsignarEmpleado(chat As ChatEntidad)
+        Try
+
+            Dim Command2 As SqlCommand = Acceso.MiComando("update Chat set ID_Empleado=@ID_Empleado where ID_Chat=@ID_Chat")
+            With Command2.Parameters
+                .Add(New SqlParameter("@ID_Chat", chat.ID))
+                .Add(New SqlParameter("@ID_Empleado", chat.Empleado.ID_Usuario))
+            End With
+            Acceso.Escritura(Command2)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Public Function AgregarMensaje(chat As ChatEntidad) As Boolean
+        Try
+
+            Dim Command2 As SqlCommand = Acceso.MiComando("insert into Mensaje_Chat (ID_Chat,ID_Usuario,Fecha,Texto) values (@ID_Chat,@ID_Usuario, @Fecha,@Texto)")
+            With Command2.Parameters
+                .Add(New SqlParameter("@ID_Chat", chat.ID))
+                .Add(New SqlParameter("@ID_Usuario", chat.MensajesChat.Last.Usuario.ID_Usuario))
+                .Add(New SqlParameter("@Fecha", chat.MensajesChat.Last.Fecha))
+                .Add(New SqlParameter("@Texto", chat.MensajesChat.Last.Texto))
+            End With
+            Acceso.Escritura(Command2)
+
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function TraerChats() As List(Of ChatEntidad)
+        Try
+            Dim consulta As String = "Select * from Chat"
+            Dim Command As SqlCommand = Acceso.MiComando(consulta)
+            Dim dt As DataTable = Acceso.Lectura(Command)
+            Dim listachat As New List(Of ChatEntidad)
+            For Each _dr As DataRow In dt.Rows
+                Dim _chat As New ChatEntidad
+                FormatearChat(_chat, _dr)
+                listachat.Add(_chat)
+            Next
+            Return listachat
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Private Function TraerMensajesChat(iD As Integer) As List(Of MensajeChatEntidad)
+        Try
+            Dim consulta As String = "Select * from Mensaje_Chat where ID_Chat = @ID_Chat"
+
+            Dim Command As SqlCommand = Acceso.MiComando(consulta)
+            With Command.Parameters
+                .Add(New SqlParameter("@ID_Chat", iD))
+            End With
+            Dim dt As DataTable = Acceso.Lectura(Command)
+            Dim listamensajeschat As New List(Of MensajeChatEntidad)
+            For Each _dr As DataRow In dt.Rows
+                Dim _mensajeentidad As New MensajeChatEntidad
+
+                FormatearMensajeChat(_mensajeentidad, _dr)
+                listamensajeschat.Add(_mensajeentidad)
+            Next
+            Return listamensajeschat
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Sub FormatearChat(ByVal chat As ChatEntidad, ByVal row As DataRow)
+        Try
+            chat.ID = row("ID_Chat")
+            chat.Usuario = (New UsuarioMPP).BuscarUsuarioID(New UsuarioEntidad With {.ID_Usuario = row("ID_Usuario")})
+            If Not IsDBNull(row("ID_Empleado")) Then
+                chat.Empleado = (New UsuarioMPP).BuscarUsuarioID(New UsuarioEntidad With {.ID_Usuario = row("ID_Empleado")})
+            Else
+                chat.Empleado = New Entidades.UsuarioEntidad With {.ID_Usuario = -2}
+            End If
+            chat.MensajesChat = TraerMensajesChat(chat.ID)
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub FormatearMensajeChat(mensajeentidad As MensajeChatEntidad, Row As DataRow)
+        mensajeentidad.ID = Row("ID_Mensaje_Chat")
+        mensajeentidad.Usuario = (New UsuarioMPP).BuscarUsuarioID(New UsuarioEntidad With {.ID_Usuario = Row("ID_Usuario")})
+        mensajeentidad.Texto = Row("Texto")
+        mensajeentidad.Fecha = Row("Fecha")
+    End Sub
+End Class
---
+++ InnovaLED/Vista/Chat.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/MasterPage.Master" CodeBehind="Chat.aspx.vb" Inherits="Vista.Chat" MaintainScrollPositionOnPostback="true" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ScriptManager ID="ScriptManager2" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server" >
+        <ContentTemplate>
+            <div class="container-fluid fondoGris">
+                <br />
+                <div id="alertvalid" runat="server" name="alertvalid" class="alert alert-danger  text-center" visible="false">
+                    <label runat="server" id="txtvalidprod" class="text-danger"></label>
+                </div>
+                <div id="success" runat="server" name="success" class="alert alert-success  text-center" visible="false">
+                    <label id="lblsuccessprod" class="text-success"></label>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-primary class">
+                            <div class="panel-heading text-center">
+                                <asp:Label ID="lblPanelModUser" runat="server" Text="Chat" Font-Size="24px" CssClass="TituloPanel"></asp:Label>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+
+
+                </div>
+
+                <div class="row col-md-12">
+                    <div class="thumbnail col-md-12">
+                        <br />
+                        <br />
+                        <div id="chats" runat="server" class="col-md-3">
+                        </div>
+                        <div id="chat" runat="server" class="col-md-9">
+
+                            <br />
+                            <br />
+                            <div id="mensajes" runat="server">
+                            </div>
+                            <br />
+                            <br />
+
+                            <div class="col-md-9">
+                                <textarea class="form-control" rows="2" id="txt_mensaje" runat="server" enable="false" enableviewstate="True" visible="False"></textarea>
+                            </div>
+                            <div class="col-md-2">
+                                <asp:Button ID="btn_mensaje" runat="server" Text="Enviar" type="button" class="btn btn-info btn-lg " Visible="False"></asp:Button>
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <asp:HiddenField ID="id_chat" runat="server" Value="-1" />
+            <asp:Timer ID="Timer1" runat="server" EnableViewState="False" Interval="5000">
+            </asp:Timer>
+        </ContentTemplate>
+
+    </asp:UpdatePanel>
+</asp:Content>
